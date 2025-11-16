596f9e78d83f559a69101717991149b123557782
yasar-beg368 <yasarmirza368@gmail.com>
2025-11-16 23:33:00 +0530

    add parenthasis

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from flask import Flask, render_template, request, jsonify
 import math
+import ast
+import operator as op
 app = Flask(__name__)
+
+
+
+def eval_expr(expr):
+    node = ast.parse(expr, mode='eval').body
+    return eval_(node)
+
+def eval_(node):
+    if isinstance(node, ast.Num):  # numbers
+        return node.n
+
+    elif isinstance(node, ast.BinOp):  # binary operations (+-*/)
+        left = eval_(node.left)
+        right = eval_(node.right)
+        return operators[type(node.op)](left, right)
+
+    elif isinstance(node, ast.UnaryOp):  # -3, +5
+        operand = eval_(node.operand)
+        return operators[type(node.op)](operand)
+
+    else:
+        raise TypeError("Invalid expression")
+
 
 def calculate_expression(expr):
     # Manual evaluation (simple support: +, -, *, /)
+    # Handle log function manually
+    if expr.startswith("log(") and expr.endswith(")"):
+        num = float(expr[4:-1])  # extract number inside parentheses
+        return math.log10(num)   # base-10 log
+
     if expr.startswith("√"):
         num = float(expr[1:])
         return math.sqrt(num)
@@ -38,6 +68,15 @@ def calculate_expression(expr):
         elif op == "^":
             result = result ** numbers[i+1]
     return result
+# Allowed operators
+operators = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+    ast.Pow: op.pow,
+    ast.USub: op.neg,
+}
 
 @app.route("/")
 def home():
